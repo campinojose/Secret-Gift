ba93b817909fd651ddad0be18bd3f64b402cc8f8
campinojose <campino.jose@correounivalle.edu.co>
2026-02-14 14:12:49 -0500

    Force .nojekyll and clean up config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/Secret-Gift",
-  assetPrefix: "/Secret-Gift",
   trailingSlash: true,
   images: {
     unoptimized: true,
